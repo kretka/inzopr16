41537b73c2276bc50c5569bd46cc186938e10678
Monika Kretek <apple@MacBook-Air-Apple.local>
2016-03-23 12:58:46 +0100

    Dodalam funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,10 @@
-def dodawanie():
-    a=int(input("Podaj a"))
-    b=int(input("Podaj b"))
-    print(a+b)
+def dodawanie(a, b):
+    return a+b
 
+def get_info():
+    return "To jest program kalkulator stworzony przeze mnie"
+
+print(get_info())
+get_info()
 dodawanie()
 input()
